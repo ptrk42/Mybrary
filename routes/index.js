52379065628a34af7e342aff20d4c9f4f5dfec87
Patrick Schimmelpfennig <patrick.schimmelpfennig@ir-logistics.eu>
2022-05-05 08:31:21 +0200

    modified:   package-lock.json 	modified:   package.json 	modified:   routes/index.js 	modified:   server.js 	modified:   views/index.ejs 	modified:   views/layouts/layout.ejs

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,8 @@ router.get('/', (req, res) => {
   res.render('index')
 })
 
+router.get('/afr', (req,res) => {
+  res.render('afr')
+})
+
 module.exports = router
--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const app = express();
 const expressLayouts = require('express-ejs-layouts');
 
 const indexRouter = require('./routes/index');
+const afrRouter = require('./routes/afr');
 
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
@@ -21,5 +22,6 @@ db.on('error', error => console.error(error));
 db.once('open', () => console.log('Connected to Mongoose'));
 
 app.use('/', indexRouter);
+app.use('/afr', afrRouter);
 
-app.listen(process.env.PORT || 3000); 
+app.listen(process.env.PORT || 3000);
